a709136a040d564e41855feabad13945da7ec48d
Jan Lukas Spaeh <janlukas.spaeh@tu-dortmund.de>
2019-11-30 17:49:19 +0100

    Starting theory for rutherford

---
+++ V16_RutherfordStreuung/plot.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy.optimize
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from scipy import optimize
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
