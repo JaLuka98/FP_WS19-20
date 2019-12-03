79dcd14b9528d958dad9f9996ef6914299083a67
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2019-12-03 07:51:57 +0100

    data

--- V16_RutherfordStreuung/plot.py
+++ V16_RutherfordStreuung/plot.py
@@ -5,3 +5,20 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+
+
+
+#au(2 µm):
+N_au1=905
+t_au1=480
+#au(4µm):
+N_au2=836
+t_au2=480
+
+#alu(3µm):
+N_alu=901
+t_alu=480
+
+#bis(1µm):
+N_bis=
+t_bis=480
