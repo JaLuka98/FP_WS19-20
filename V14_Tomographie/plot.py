f94d8d262ce6ca1ffeefe350227481d8417d5da0
Jan Lukas Spaeh <janlukas.spaeh@tu-dortmund.de>
2019-12-16 12:29:00 +0100

    Matrix in der plot.py, Theorietabelle V14

--- V14_Tomographie/plot.py
+++ V14_Tomographie/plot.py
@@ -5,3 +5,11 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+
+s2 = 2
+A1 = np.matrix([[1,1,1,0,0,0,0,0,0],[0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,1,1,1]])
+A2 = np.matrix([[1,0,0,1,0,0,1,0,0],[0,1,0,0,1,0,0,1,0],[0,0,1,0,0,1,0,0,1]])
+A3 = np.matrix([[0,s2,0,s2,0,0,0,0,0],[0,0,s2,0,s2,0,s2,0,0],[0,0,0,0,0,s2,0,s2,0]])
+A4 = np.matrix([[0,0,0,s2,0,0,0,s2,0],[s2,0,0,0,s2,0,0,0,s2],[0,s2,0,0,0,s2,0,0,0]])
+A = np.vstack((A1,A2,A3,A4))
+print(np.linalg.inv(A.T@A))
