b2974bcd3ce126de589739c64735359acf58463a
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2019-12-17 10:59:12 +0100

    .

--- V14_Tomographie/plot.py
+++ V14_Tomographie/plot.py
@@ -6,6 +6,41 @@ from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
 
+t=180
+t_0=300
+t_1=120
+
+I_0= unp.uarray([55224], [262])/300
+print(I_0)
+
+####################
+######Spektrum######
+####################
+
+#Muss man anscheinend gegen einen linspace auftragen weil es einfach keine x-Werte gibt
+
+
+#####################
+######Wuerfel 1######
+#####################
+
+
+
+#####################
+######Wuerfel 2######
+#####################
+
+
+
+#####################
+######Wuerfel 3######
+#####################
+
+
+
+#####################
+######Wuerfel 4######
+#####################
 s2 = 2
 A1 = np.matrix([[1,1,1,0,0,0,0,0,0],[0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,1,1,1]])
 A2 = np.matrix([[1,0,0,1,0,0,1,0,0],[0,1,0,0,1,0,0,1,0],[0,0,1,0,0,1,0,0,1]])
